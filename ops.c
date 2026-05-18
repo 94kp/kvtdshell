0340950cf7838d4bcb526c7fe9ab9e5181f17f7c
Tanish Mahajan <mahajantanish28@gmail.com>
2026-05-18 08:10:16 -0400

    Implement cd command and append redirection

--- ops.c
+++ ops.c
@@ -4,8 +4,31 @@
 #include <unistd.h>
 #include <fcntl.h>
 
-int main(int argc, char *argv[])
+int main(int argc, char* argv[])
 {
+    (void)argc;
+
+    char helper_dir[1024] = ".";
+    char mkdir_path[1200];
+    char rmdir_path[1200];
+
+    /* shell.c executes ops using its absolute path.  Use that path to find
+       helper binaries even after cd changes the current working directory. */
+    char *last_slash = strrchr(argv[0], '/');
+    if (last_slash != NULL)
+    {
+        size_t dir_len = last_slash - argv[0];
+        if (dir_len >= sizeof(helper_dir))
+        {
+            dir_len = sizeof(helper_dir) - 1;
+        }
+        strncpy(helper_dir, argv[0], dir_len);
+        helper_dir[dir_len] = '\0';
+    }
+
+    snprintf(mkdir_path, sizeof(mkdir_path), "%s/mkdir", helper_dir);
+    snprintf(rmdir_path, sizeof(rmdir_path), "%s/rmdir", helper_dir);
+
     char **args = &argv[1];
 
     if (args[0] == NULL)
@@ -13,102 +36,86 @@ int main(int argc, char *argv[])
         return 0;
     }
 
-    for (int i = 0; args[i] != NULL; i++)
+    for (int i = 0; args[i] != NULL; i++) 
     {
+        int append_mode = 0;
+
         if (strcmp(args[i], ">") == 0)
         {
-            if (args[i + 1] == NULL)
-            {
-                fprintf(stderr, "Syntax error: Redirection target not found\n");
-                exit(EXIT_FAILURE);
-            }
-
-            char *filename = args[i + 1];
-
-            int fd = open(filename, O_WRONLY | O_CREAT | O_TRUNC, 0644);
-            if (fd < 0)
-            {
-                perror("Error: Output file could not be opened");
-                exit(EXIT_FAILURE);
-            }
-
-            if (dup2(fd, STDOUT_FILENO) < 0)
-            {
-                perror("Error: Redirection failed");
-                exit(EXIT_FAILURE);
-            }
-
-            close(fd);
-
-            args[i] = NULL;
-            break;
+            append_mode = 0;
         }
-
-        if (strcmp(args[i], "<") == 0)
+        else if (strcmp(args[i], ">>") == 0)
         {
-            if (args[i + 1] == NULL)
-            {
-                fprintf(stderr, "Syntax error: Redirection source not found\n");
-                exit(EXIT_FAILURE);
-            }
-
-            char *filename = args[i + 1];
-
-            int fd = open(filename, O_RDONLY);
-            if (fd < 0)
-            {
-                perror("Error: Input file could not be opened");
-                exit(EXIT_FAILURE);
-            }
-
-            if (dup2(fd, STDIN_FILENO) < 0)
-            {
-                perror("Error: Redirection failed");
-                exit(EXIT_FAILURE);
-            }
-
-            close(fd);
-            args[i] = NULL;
-            break;
+            append_mode = 1;
         }
+        else
+        {
+            continue;
+        }
+
+        if (args[i + 1] == NULL)
+        {
+            fprintf(stderr, "Syntax error: Redirection target not found\n");
+            exit(EXIT_FAILURE);
+        }
+
+        char *filename = args[i + 1];
+
+        int flags = O_WRONLY | O_CREAT;
+
+        if (append_mode)
+        {
+            flags |= O_APPEND;
+        }
+        else
+        {
+            flags |= O_TRUNC;
+        }
+
+        int fd = open(filename, flags, 0644);
+        if (fd < 0)
+        {
+            perror("Error: Output file could not be opened");
+            exit(EXIT_FAILURE);
+        }
+
+        if (dup2(fd, STDOUT_FILENO) < 0)
+        {
+            perror("Error: Redirection failed");
+            exit(EXIT_FAILURE);
+        }
+
+        close(fd);
+
+        args[i] = NULL;
+        break;
     }
 
-    if (strcmp(args[0], "mkdir") == 0)
+    if (strcmp(args[0], "mkdir") == 0) 
     {
-        args[0] = "./mkdir";
+        args[0] = mkdir_path;
     }
 
-    else if (strcmp(args[0], "rmdir") == 0)
+    else if (strcmp(args[0], "rmdir") == 0) 
     {
-        args[0] = "./rmdir";
+        args[0] = rmdir_path;
     }
 
-    else if (strcmp(args[0], "ls") == 0)
-    {
-        args[0] = "./ls";
-    }
-
-    else if (strcmp(args[0], "touch") == 0)
-    {
-        args[0] = "./touch";
-    }
-
-    else if (strcmp(args[0], "cat") == 0)
-    {
-        args[0] = "./cat";
-    }
+    /* Do not rewrite ls to ./ls because this project does not build a local
+       ls binary. Leaving it as "ls" lets execvp find the system ls. */
 
     // add more commands here as and when they are implemented
 
-    // else if (strcmp(args[0], "rmdir") == 0)
+    // else if (strcmp(args[0], "rmdir") == 0) 
     // {
     //     args[0] = "./rmdir";
     // }
 
-    if (execvp(args[0], args) == -1)
+    if (execvp(args[0], args) == -1) 
     {
         perror("Error: Command execution failed");
     }
 
     return 0;
 }
+
--- shell.c
+++ shell.c
@@ -1,109 +1,174 @@
 #include <stdio.h>
 #include <string.h>
 #include <stdlib.h>
-#include <unistd.h> 
-#include <sys/wait.h> 
+#include <unistd.h>
+#include <sys/wait.h>
+#include <limits.h>
+#include <errno.h>
 
 #define MAXSIZE 99
 #define MAXARGS 16
 
+void tokenizer(char *input, char **args);
+void handle_cd(char **args);
+
+static char prev_dir[PATH_MAX] = "";
+static char shell_start_dir[PATH_MAX] = "";
+
 int main ()
 {
-	// function signatures
-	//void list_files(int, char*);
-	void tokenizer(char *input, char **args);
-
 	char selector = 'c';
 	char u_input[MAXSIZE] = "sample";
 	char *args[MAXARGS];
-
 	char prompt[MAXSIZE] = "> ";
 
-	// implement tokenizer here`
+	if (getcwd(shell_start_dir, sizeof(shell_start_dir)) == NULL)
+	{
+		perror("getcwd");
+		return EXIT_FAILURE;
+	}
 	
 	while(selector != EOF)
 	{
 		printf("%s", prompt);
 		fflush(stdout);
-		// scanf("%s", u_input); // not the best option to use here
 
-		if (fgets(u_input, sizeof(u_input), stdin) == NULL) 
+		if (fgets(u_input, sizeof(u_input), stdin) == NULL)
 		{
-            printf("\n");
-            break;
-        }
-		
-		// printf("\n");
+			printf("\n");
+			break;
+		}
 
-		// run tokenizer here to figure out what command has been entered.
-
-		// int cmd_num = tokenizer(u_input);
 		tokenizer(u_input, args);
 
-		if (args[0] == NULL) 
+		if (args[0] == NULL)
 		{
-            continue;
-        }
+			continue;
+		}
 
-		if (strcmp(args[0], "exit") == 0) 
+		if (strcmp(args[0], "exit") == 0)
 		{
-            break;
-        }
+			break;
+		}
 
+		/* cd must be handled as a built-in command because it changes
+		   the current directory of the shell process itself. */
 		if (strcmp(args[0], "cd") == 0)
-        {
-            if (args[1] == NULL) 
-            {
-                fprintf(stderr, "cd: missing arguments, require path\n");
-            } 
-            else if (chdir(args[1]) != 0) 
-            {
-                perror("Operation Failed");
-            }
-            continue;
-        }
+		{
+			handle_cd(args);
+			continue;
+		}
 
-		// pid_t pid = fork();
+		pid_t pid = fork();
 
-        pid_t pid = fork();
+		if (pid < 0)
+		{
+			perror("fork() failed");
+		}
+		else if (pid == 0)
+		{
+			for (int i = MAXARGS - 1; i > 0; i--)
+			{
+				args[i] = args[i - 1];
+			}
 
-        if (pid < 0) {
-            perror("fork() failed");
-        } 
-        else if (pid == 0) 
-        {
+			char ops_path[PATH_MAX];
+			if (strlen(shell_start_dir) + strlen("/ops") >= sizeof(ops_path))
+			{
+				fprintf(stderr, "Error: shell path is too long\n");
+				exit(EXIT_FAILURE);
+			}
+			strcpy(ops_path, shell_start_dir);
+			strcat(ops_path, "/ops");
+			args[0] = ops_path;
 
-            for (int i = MAXARGS - 1; i > 0; i--) {
-                args[i] = args[i - 1];
-            }
-            args[0] = "./ops"; 
-
-            if (execvp(args[0], args) == -1) 
-            {
-                perror("failed to initialise parser for operators");
-            }
-            exit(EXIT_FAILURE);
-        } 
-        else 
-        {
-            int status;
-            waitpid(pid, &status, 0);
-        }
-    }
+			if (execvp(args[0], args) == -1)
+			{
+				perror("failed to initialise parser for operators");
+			}
+			exit(EXIT_FAILURE);
+		}
+		else
+		{
+			int status;
+			waitpid(pid, &status, 0);
+		}
+	}
 
 	return 0;
 }
 
-void tokenizer(char *input, char **args) 
+void handle_cd(char **args)
 {
-    int i = 0;
-    char *token = strtok(input, " \t\n\r");
-    
-    while (token != NULL && i < MAXARGS - 1) 
+	char current_dir[PATH_MAX];
+	char target_dir[PATH_MAX];
+	char *target = NULL;
+
+	if (args[2] != NULL)
 	{
-        args[i++] = token;
-        token = strtok(NULL, " \t\n\r");
-    }
-    args[i] = NULL;
+		fprintf(stderr, "cd: too many arguments\n");
+		return;
+	}
+
+	if (getcwd(current_dir, sizeof(current_dir)) == NULL)
+	{
+		perror("cd");
+		return;
+	}
+
+	if (args[1] == NULL || strcmp(args[1], "~") == 0)
+	{
+		target = getenv("HOME");
+		if (target == NULL)
+		{
+			fprintf(stderr, "cd: HOME not set\n");
+			return;
+		}
+	}
+	else if (strcmp(args[1], "-") == 0)
+	{
+		if (prev_dir[0] == '\0')
+		{
+			fprintf(stderr, "cd: OLDPWD not set\n");
+			return;
+		}
+		target = prev_dir;
+	}
+	else if (strncmp(args[1], "~/", 2) == 0)
+	{
+		char *home = getenv("HOME");
+		if (home == NULL)
+		{
+			fprintf(stderr, "cd: HOME not set\n");
+			return;
+		}
+		snprintf(target_dir, sizeof(target_dir), "%s/%s", home, args[1] + 2);
+		target = target_dir;
+	}
+	else
+	{
+		target = args[1];
+	}
+
+	if (chdir(target) != 0)
+	{
+		perror("cd");
+		return;
+	}
+
+	strncpy(prev_dir, current_dir, sizeof(prev_dir) - 1);
+	prev_dir[sizeof(prev_dir) - 1] = '\0';
 }
 
+void tokenizer(char *input, char **args)
+{
+	int i = 0;
+	char *token = strtok(input, " \t\n\r");
+	
+	while (token != NULL && i < MAXARGS - 1)
+	{
+		args[i++] = token;
+		token = strtok(NULL, " \t\n\r");
+	}
+	args[i] = NULL;
+}
